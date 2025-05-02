2801cb649b31f9e793f0742dc3f9b5070223b330
TatkoSmollko <tomas.vago2@gmail.com>
2025-05-02 14:38:52 +0200

    remove unused

--- src/test/java/com/divsoft/transactionService/FeeCalculatorFactoryTest.java
+++ src/test/java/com/divsoft/transactionService/FeeCalculatorFactoryTest.java
@@ -1,56 +1,53 @@
-//package com.divsoft.transactionService;
-//
-//import com.divsoft.transactionService.model.Fee;
-//import com.divsoft.transactionService.service.FeeCalculator;
-//import com.divsoft.transactionService.service.impl.CombineFeeCalculator;
-//import com.divsoft.transactionService.service.impl.FeeCalculatorFactory;
-//import com.divsoft.transactionService.service.impl.FixedFeeCalculator;
-//import com.divsoft.transactionService.service.impl.PercentFeeCalculator;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//
-//public class FeeCalculatorFactoryTest {
-//
-//    @Test
-//    void whenFixedFee_thenReturnFixedFeeCalculator() {
-//        Fee fee = new Fee();
-//        fee.setFixedFee(10.0);
-//
-//        FeeCalculator calculator = FeeCalculatorFactory.getCalculator(fee);
-//
-//        assertInstanceOf(FixedFeeCalculator.class, calculator);
-//    }
-//
-//    @Test
-//    void whenPercentFee_thenReturnPercentFeeCalculator() {
-//        Fee fee = new Fee();
-//        fee.setPercentFee(0.1);
-//
-//        FeeCalculator calculator = FeeCalculatorFactory.getCalculator(fee);
-//
-//        assertInstanceOf(PercentFeeCalculator.class, calculator);
-//    }
-//
-//    @Test
-//    void whenBothFees_thenReturnCombinedFeeCalculator() {
-//        Fee fee = new Fee();
-//        fee.setFixedFee(10.0);
-//        fee.setPercentFee(0.1);
-//
-//        FeeCalculator calculator = FeeCalculatorFactory.getCalculator(fee);
-//
-//        assertInstanceOf(CombineFeeCalculator.class, calculator);
-//    }
-//
-//    @Test
-//    void whenNoFees_thenThrowException() {
-//        Fee fee = new Fee(); // no fees set
-//
-//        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-//            FeeCalculatorFactory.getCalculator(fee);
-//        });
-//
-//        assertEquals("Unknown fee type", exception.getMessage());
-//    }
-//}
+package com.divsoft.transactionService;
+
+import com.divsoft.transactionService.model.Fee;
+
+import com.divsoft.transactionService.service.impl.Calculator.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class FeeCalculatorFactoryTest {
+
+    @Test
+    void whenFixedFee_thenReturnFixedFeeCalculator() {
+        Fee fee = new Fee();
+        fee.setFixedFee(10.0);
+
+        FeeCalculator calculator = FeeCalculatorFactory.getCalculator(fee);
+
+        assertInstanceOf(FixedFeeCalculator.class, calculator);
+    }
+
+    @Test
+    void whenPercentFee_thenReturnPercentFeeCalculator() {
+        Fee fee = new Fee();
+        fee.setPercentFee(0.1);
+
+        FeeCalculator calculator = FeeCalculatorFactory.getCalculator(fee);
+
+        assertInstanceOf(PercentFeeCalculator.class, calculator);
+    }
+
+    @Test
+    void whenBothFees_thenReturnCombinedFeeCalculator() {
+        Fee fee = new Fee();
+        fee.setFixedFee(10.0);
+        fee.setPercentFee(0.1);
+
+        FeeCalculator calculator = FeeCalculatorFactory.getCalculator(fee);
+
+        assertInstanceOf(CombineFeeCalculator.class, calculator);
+    }
+
+    @Test
+    void whenNoFees_thenThrowException() {
+        Fee fee = new Fee(); // no fees set
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            FeeCalculatorFactory.getCalculator(fee);
+        });
+
+        assertEquals("Unknown fee type", exception.getMessage());
+    }
+}
